69b3e46ed46623fcecedf48284f79e65326a0ff4
Nikhil.Agrawal <nikhil.agrwl07@gmail.com>
2019-08-16 18:10:30 -0500

    added design distributed cache using hashMap and consistent hashing logic

---
+++ src/main/java/ConsistentHashing/MapClient.java
@@ -0,0 +1,15 @@
+package ConsistentHashing;
+
+public interface MapClient {
+
+    public boolean addServerToHashRing(Server server);
+
+    public boolean removeServerToHashRing(Server server);
+
+    public Server getServerForGivenKey(String key);
+
+    public Boolean put(String k, String v);
+
+    public Entry get(String k);
+
+}
